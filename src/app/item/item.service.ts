8ba0dbdc49839ccf1fc068fbc721b15a5fa12561
kevin <kbarriosc8@miumg.edu.com>
2021-09-25 22:53:25 -0600

    Initial commit "EXAMEN 2"

---
+++ src/app/item/item.service.ts
@@ -0,0 +1,18 @@
+import { Injectable } from '@angular/core'
+
+import { Item } from './item'
+import {HttpClient} from "@angular/common/http";
+
+@Injectable({
+  providedIn: 'root',
+})
+export class ItemService {
+  constructor(private http: HttpClient){}
+
+
+  getItems(){
+    return this.http.get("https://api.nasa.gov/mars-photos/api/v1/rovers/curiosity/photos?sol=1000&page=2&api_key=DEMO_KEY")
+  }
+}
+
+
